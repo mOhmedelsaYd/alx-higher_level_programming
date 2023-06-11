9062ef43eef6d66e4d0e1cbe8021c9484b931101
mOhmedelsaYd <mohamedelsaid3963@gmail.com>
2023-06-11 08:59:53 -0700

    mo

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for number in range(0,99):
-    print("{:02}".format(number), end =", ")
-print("99")
+for number in range(0,100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end =", ")
+
