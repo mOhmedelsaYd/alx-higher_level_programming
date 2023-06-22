acccdb7b5fbd57547e7fb21bb018788c8b13341d
mOhmedelsaYd <mohamedelsaid3963@gmail.com>
2023-06-22 06:29:04 -0700

    mo

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,10 +2,8 @@
 
 if __name__ == "__main__":
 
-
     from calculator_1 import add, sub, mul, div
-    """Print sum, difference, multiple, div mo mo mo mo mo mo mo mo mo.
-"""
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
     a = 10
     b = 5
 
