6264edff503efa9c23f16f872fdef8e9aec667a6
mOhmedelsaYd <mohamedelsaid3963@gmail.com>
2023-06-24 09:59:17 -0700

    mo

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@
 
 def element_at(my_list, idx):
     """Return element in the determined index"""
-    if(idx < 0 or idx > (len(my_list) - 1)):
+    if idx < 0 or idx > (len(my_list) - 1):
         return None
     return (my_list[idx])
