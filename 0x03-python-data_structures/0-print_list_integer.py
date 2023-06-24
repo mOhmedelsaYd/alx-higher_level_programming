6796243975966a9724408e7a940b975234f26b84
mOhmedelsaYd <mohamedelsaid3963@gmail.com>
2023-06-24 09:33:13 -0700

    mo

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 0-print_list_integer.py
+
+def print_list_integer(my_list=[]):
+    """pirnt all element of list"""
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
